d812301ead4ee6f9bccf97f3f5cd5617b2888c57
Aleksander Szymański <bitterisland6@gmail.com>
2023-11-17 16:37:36 +0100

    move child_frame_id to a parameter
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- src/odom2tf.cpp
+++ src/odom2tf.cpp
@@ -3,6 +3,7 @@
 #include <nav_msgs/Odometry.h>
 
 static std::string topic = "odometry_merged";
+static std::string child_frame = "base_footprint";
 
 void odom_callback(const nav_msgs::OdometryConstPtr& odom){
   static tf::TransformBroadcaster br;
@@ -10,7 +11,7 @@ void odom_callback(const nav_msgs::OdometryConstPtr& odom){
   geometry_msgs::Pose odom_pose = odom->pose.pose;
   tf::poseMsgToTF(odom_pose, tf);
 
-  tf::StampedTransform stamped_tf(tf, odom->header.stamp, odom->header.frame_id, "base_footprint");
+  tf::StampedTransform stamped_tf(tf, odom->header.stamp, odom->header.frame_id, child_frame);
 
   br.sendTransform(stamped_tf);
 }
@@ -22,6 +23,7 @@ int main(int argc, char **argv){
   ros::NodeHandle pnh("~");
 
   pnh.getParam("odom_topic", topic);
+  pnh.getParam("child_frame_id", child_frame);
 
   ros::Subscriber odom_sub = nh.subscribe(topic, 10, odom_callback);
   ros::spin();
